617e29b5c41fe70e89fc123afa03cf527665c567
matiasezequieldimuro <mdimuro@hexagonconsulting.net>
2024-12-04 14:36:33 -0300

    Add unit tests w/Jest

---
+++ test/.test.js
@@ -0,0 +1,31 @@
+const cds = require('@sap/cds')
+
+describe("Licenses Service REST API Test with Jest", ()=>{
+    
+	const { GET } = cds.test(__dirname+'/..');
+
+	const users = {
+		admin: {
+			auth: {
+				username: "admin@email.com",
+				password: "123"
+			}
+		}
+	}
+    
+	it ("'Hello World' message is expected", () => {
+		let message = "Hello World";
+		expect(message).toBe("Hello World");
+    });
+
+	it ("Check LicenseType entity with ID 1 is 'Holidays'", async () => {
+		// Arrange: set initial data or input.
+		let ID = 1;
+		// Act: do what we want to test.
+		let res = await GET(`/service/licenses/LicenseTypes/${ID}`, users.admin);
+		// Verify: check output is correct given input.
+		expect(res.data.name).toBe("Holidays");
+		expect(res.data.ID).toBe(ID);
+		expect(res.status).toBe(200);
+    });
+})
